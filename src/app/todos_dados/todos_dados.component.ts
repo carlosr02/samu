62a99c93ce854f4c0452fd69b536b41ae6e4b2b2
Carlos Romero <carlosr-02@live.com>
2017-07-18 16:31:46 -0300

    TODO: trocar o id pelo nome

--- src/app/todos_dados/todos_dados.component.ts
+++ src/app/todos_dados/todos_dados.component.ts
@@ -1,8 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { Location } from '@angular/common';
 
+import { UF } from '../types/uf';
 import { Dados } from '../types/samu';
 import { SamuService } from '../services/samu.service';
+import { UFService } from '../services/uf.service';
+
+class Municipio{
+  valor: number;
+  uf_nome: UF;
+  ano: number;
+
+  constructor(valor, uf_nome, ano){
+    this.valor = valor;
+    this.uf_nome = uf_nome;
+    this.ano = ano;
+  }
+}
 
 @Component({
   selector: 'todos_dados',
@@ -10,16 +24,23 @@ import { SamuService } from '../services/samu.service';
   styleUrls: ['./todos_dados.component.css']
 })
 export class TodosDadosComponent implements OnInit {
-  municipios: Dados[];
+  municipios: Municipio[];
 
-  constructor(private samuService: SamuService, private location: Location) { }
+  constructor(private samuService: SamuService, private ufService: UFService,
+    private location: Location) { }
 
   ngOnInit() : void {
     this.getDados();
   }
 
   getDados() : void {
-    this.samuService.getAllMunicipiosAtendidosPorEstado().then(municipios => this.municipios = municipios);
+    this.samuService.getAllMunicipiosAtendidosPorEstado()
+      .then(municipios => municipios
+        .forEach(municipio => this.municipios
+          .push(new Municipio(
+            municipio.valor,
+            this.ufService.getPorId(municipio.uf_id),
+            municipio.ano))));
   }
 
   goBack(): void {
